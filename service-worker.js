3284914e2a47af77830f6baf0da4425a6daa518c
upjobalerts <akutundla@gmail.com>
2021-02-21 00:43:29 +0530

    manifest color changed to white

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,13 @@ if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
 
   workbox.precaching.precacheAndRoute([]);
+  import {registerRoute} from 'workbox-routing';
+import {CacheFirst} from 'workbox-strategies';
+
+registerRoute(
+  ({request}) => request.destination === 'style' ,
+  new CacheFirst()
+);
 
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
