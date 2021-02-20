7d19a2ad1f4533246e5a83c872bb200c7a8b3216
upjobalerts <akutundla@gmail.com>
2021-02-21 00:26:15 +0530

    rtyguhji

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,10 @@
-console.log('Hello from service-worker.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+
+  workbox.precaching.precacheAndRoute([]);
+
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
